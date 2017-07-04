a99ea729f986789947bb9386f6c2d30b91fa394b
john-mcgowan1992 <mcgowanj92@gmail.com>
2017-07-03 17:38:27 -0700

    headcount backend implemented

--- server/employees.py
+++ server/employees.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 raw_data = [{'date': '2017-03-01', 'dept': 'Sales', 'employee': 3, 'salary': 70000},
  {'date': '2015-03-01', 'dept': 'Engineering', 'employee': 4, 'salary': 45000},
@@ -52,10 +53,9 @@ def format_list(employee_list):
             list_cleaned[key][date_sub_key] = new_entry
         else:
             list_cleaned[key][date_sub_key] = new_entry
-    print list_cleaned
     return list_cleaned
 
-def addByDepartment(empID, empObj):
+def return_current_employee(empID, empObj):
     empStartDates = list(empObj.keys())
     if len(empStartDates) == 1:
         emp = empObj[empStartDates[0]]
@@ -72,7 +72,7 @@ def addByDepartment(empID, empObj):
 def current_employee_stats(employee_dict):
     employees_dept = {}
     for employeeID in employee_dict:
-        new_emp = addByDepartment(employeeID, employee_dict[employeeID])
+        new_emp = return_current_employee(employeeID, employee_dict[employeeID])
         if not new_emp.department in employees_dept:
             employees_dept[new_emp.department] = list()
             employees_dept[new_emp.department].append(new_emp)
@@ -85,9 +85,10 @@ class Company:
         self.name = name
         self.employee_dictionary = format_list(list)
         self.employee_count = len(self.employee_dictionary)
+        self.date_founded = min([emp['date'] for emp in list])
     
     def __repr__(self):
-        return "<Company: %r, employees: %r >" % (self.name, self.employee_count)
+        return "<Company: %r, employees: %r, founded: %r >" % (self.name, self.employee_count, self.date_founded)
 
 class Employee:
     def __init__(self, id, start_date, department, salary):
@@ -101,3 +102,29 @@ class Employee:
 
 EMPLOYEES = Company("Starling", raw_data)
 EMPLOYEES_FORMATTED = current_employee_stats(EMPLOYEES.employee_dictionary)
+print EMPLOYEES
+
+def department_headcount(company):
+    headcount_list = list()
+    start_date = datetime.strptime(company.date_founded, "%Y-%m-%d")
+    emp_dict = company.employee_dictionary
+    for i in range(36):
+        month_dict = dict()
+        current_month = start_date + relativedelta(months=+i)
+        month_dict['month'] = datetime.strftime(current_month, "%Y-%m-%d")
+        month_dict['Engineering'] = 0
+        month_dict['Sales'] = 0
+        month_dict['Support'] = 0
+        headcount_list.append(month_dict)
+    for month_count in headcount_list:
+        current = month_count['month']
+        for emp_id in company.employee_dictionary:
+            employee_dates = list(company.employee_dictionary[emp_id].keys())
+            relevant_dates = [dt for dt in employee_dates if dt <= current]
+            if relevant_dates:
+                current_position_start = max(relevant_dates)
+                dept = company.employee_dictionary[emp_id][current_position_start][0]
+                month_count[dept] += 1
+    return headcount_list
+
+print department_headcount(EMPLOYEES)
